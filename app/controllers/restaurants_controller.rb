6558298e9a3c26564e544d24171977e83d0d6913
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-07 15:01:58 +0200

    avant menu déroulant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,39 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    set_restaurant
+    @phone = phone_number?
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def phone_number?
+    if @restaurant.phone_number
+      return @restaurant.phone_number
+    else
+      return "The phone number of this restaurant has not been given"
+    end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
